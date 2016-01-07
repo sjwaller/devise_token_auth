9981a11e20475a7118514e7dc33de0d7c9fb8ce7
Stewart Waller <sjwaller@gmail.com>
2016-01-07 11:34:40 +0000

    add `token_validation_response` method to user concern

--- app/controllers/devise_token_auth/token_validations_controller.rb
+++ app/controllers/devise_token_auth/token_validations_controller.rb
@@ -5,12 +5,11 @@ module DeviseTokenAuth
 
     def validate_token
       # @resource will have been set by set_user_token concern
+      # @resource will have been set by set_user_token concern
       if @resource
         render json: {
           success: true,
-          data: @resource.as_json(except: [
-            :tokens, :created_at, :updated_at
-          ])
+          data: @resource.token_validation_response
         }
       else
         render json: {
